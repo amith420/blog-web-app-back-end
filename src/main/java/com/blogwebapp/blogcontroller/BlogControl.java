c9cc2541f675c853f122bc6b87c42e508285d2b4
amith-krishnan <amithkrishnan98@gmail.com>
2022-11-10 14:59:33 +0530

    first commit

--- src/main/java/com/blogwebapp/blogcontroller/BlogControl.java
+++ src/main/java/com/blogwebapp/blogcontroller/BlogControl.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogwebapp.blogmodel.Blog;
@@ -45,7 +43,6 @@ public class BlogControl {
     }
 
     @DeleteMapping("/delete/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable String id){
         blogServ.delete(id);
     }
