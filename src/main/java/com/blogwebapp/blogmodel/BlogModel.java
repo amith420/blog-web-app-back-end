63aaa7b0ea86dd8da93660d5a87edf6f0db089ca
amith-krishnan <amithkrishnan98@gmail.com>
2022-11-11 17:23:03 +0530

    renamed

--- src/main/java/com/blogwebapp/blogmodel/BlogModel.java
+++ src/main/java/com/blogwebapp/blogmodel/BlogModel.java
@@ -12,7 +12,7 @@ import lombok.Data;
 
 public class BlogModel {
     @Id
-    private String _id;
+    private String id;
     private String title;
     private String content;
     private Date date;
--- src/main/java/com/blogwebapp/blogmodel/UserModel.java
+++ src/main/java/com/blogwebapp/blogmodel/UserModel.java
@@ -10,9 +10,9 @@ import lombok.Data;
 
 public class UserModel {
     @Id
-    private String _id;
+    private String id;
     private String name;
     private String email;
-    private String dob;
+    private String dateofbirth;
     private String password;
 }
